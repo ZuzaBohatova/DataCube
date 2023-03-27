f616057d339482626c20fbac6587937870ff5202
Zuzana Bohatová <101879462+ZuzaBohatova@users.noreply.github.com>
2023-03-27 19:27:56 +0200

    Add files via upload

--- airflow/dags/dataCube_dag.py
+++ airflow/dags/dataCube_dag.py
@@ -6,21 +6,18 @@ from scripts.careProviders import produce_data_cube_CareProviders
 from scripts.population2021 import produce_data_cube_Population
 
 import pandas as pd
-import os
+import requests
 
 def set_outputPath(fce, **param):
-    output_path = param['dag_run'].conf.get("output_path", None)
-    if output_path:
-        fce(output_path)
-    else:
-        fce()
+    output_path = param['dag_run'].conf.get("output_path", "./")
+    fce(output_path)
 
 
 default_args = {
     "owner": "Zuzana Bohatova",
     "depends_on_past": False,
     "start_date": datetime(2023, 3, 27),
-    "email": ["airflowadmin@example.com"],
+    "email": ["zuzana.bohatov@email.cz"],
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
@@ -30,7 +27,7 @@ default_args = {
 with  DAG(
     dag_id="data-cubes",
     default_args=default_args,
-    description="A DAG to produce a data cube",
+    description="A DAG to produce a data cube population.ttl and health-care.ttl",
     schedule=None,
 ) as dag:
 
@@ -41,28 +38,26 @@ with  DAG(
         df = pd.read_csv("https://data.mzcr.cz/distribuce/63/narodni-registr-poskytovatelu-zdravotnich-sluzeb.csv", dtype=dtype)
         newFile = df.groupby(["Okres", "Kraj", "OborPece"]).size().reset_index(name="PocetPoskytovatelu")
         newFile.to_csv("./preparedDataCR.csv")
-        return newFile
 
     prepare_data_CareProviders_task = PythonOperator(
 		task_id = "prepare_data_CareProviders_task",
-		python_callable = prepare_data_CareProviders,
+		python_callable = prepare_data_CareProviders
 	)
     produce_data_cube_CareProviders_task = PythonOperator(
 		task_id = "produce_data_cube_CareProviders_task",
 		python_callable = set_outputPath,
-        op_args=[produce_data_cube_CareProviders],
+        op_args=[produce_data_cube_CareProviders]
 	)
 
     #Population2021
 
     def prepare_data_Population():
-        import ssl
-        import urllib.request
+        url = "https://skoda.projekty.ms.mff.cuni.cz/ndbi046/seminars/02/%C4%8D%C3%ADseln%C3%ADk-okres%C5%AF-vazba-101-nad%C5%99%C3%ADzen%C3%BD.csv"  
+        filename = url.split("/")[-1]
+        response = requests.get(url, verify=False)
+        open(filename, "wb").write(response.content)
+        codelist =pd.read_csv(filename)
 
-        ssl._create_default_https_context = ssl._create_unverified_context
-        url = "https://skoda.projekty.ms.mff.cuni.cz/ndbi046/seminars/02/%C4%8D%C3%ADseln%C3%ADk-okres%C5%AF-vazba-101-nad%C5%99%C3%ADzen%C3%BD.csv"
-        with urllib.request.urlopen(url) as response:
-            codelist = pd.read_csv(response)
         dtPopulation = pd.read_csv("https://www.czso.cz/documents/10180/184344914/130141-22data2021.csv")
 
         vukDEM0004 = dtPopulation[dtPopulation["vuk"] == "DEM0004"]
@@ -74,18 +69,17 @@ with  DAG(
 
         preparedData.rename(columns={"text1": "kraj", "text2":"okres"}, inplace=True)
         preparedData.to_csv("./preparedDataPopulation2021.csv")
-        return preparedData
     
 
     prepare_data_Population_task = PythonOperator(
 		task_id = "prepare_data_Population_task",
-		python_callable = prepare_data_Population,
+		python_callable = prepare_data_Population
 	)
 
     produce_data_cube_Population_task = PythonOperator(
 		task_id = "produce_data_cube_Population_task",
 		python_callable = set_outputPath,
-        op_args=[produce_data_cube_Population],
+        op_args=[produce_data_cube_Population]
 	)
 
     prepare_data_CareProviders_task >> produce_data_cube_CareProviders_task
